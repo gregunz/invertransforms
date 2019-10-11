8c3827471bead35d8fd0aee450f1424e3088dde8
Gregoire Clement <gregoire.clement@epfl.ch>
2019-10-11 23:46:31 +0900

    UPDATE TransformIf simplified

---
+++ transforms/transform_if.py
@@ -0,0 +1,20 @@
+from transforms.util import Invertible, _Invertible
+from . import Identity
+
+
+class TransformIf(Invertible):
+    def __init__(self, transform, condition: bool):
+        if condition:
+            assert isinstance(transform, _Invertible), f'{transform.__class__.__name__} is not an invertible class'
+            self.transform = transform
+        else:
+            self.transform = Identity()
+
+    def __call__(self, img):
+        return self.transform.__call__(img)
+
+    def __repr__(self):
+        return self.transform.__repr__()
+
+    def _invert(self, **kwargs):
+        return self.transform._invert(**kwargs)
