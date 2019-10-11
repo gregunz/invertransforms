bd8002f6308e9b5c6b973621febfb01a4dccacd0
gregunz <mail@gregunz.io>
2019-10-11 18:50:35 +0900

    ADD Normalize transformation

---
+++ transforms/normalize.py
@@ -0,0 +1,13 @@
+import torch
+from torchvision import transforms
+
+from transforms.util import Invertible
+
+
+class Normalize(transforms.Normalize, Invertible):
+    def _invert(self):
+        mean = torch.as_tensor(self.mean)
+        std = torch.as_tensor(self.std)
+        std_inv = 1 / (std + 1e-7)
+        mean_inv = (-1) * mean * std_inv
+        return Normalize(mean=mean_inv, std=std_inv, inplace=self.inplace)
