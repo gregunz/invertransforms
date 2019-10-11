27948cabced1313593a9ecaa96e05d8714d8191a
Gregoire Clement <gregoire.clement@epfl.ch>
2019-10-11 23:46:44 +0900

    REFACTOR

--- transforms/__init__.py
+++ transforms/__init__.py
@@ -1,7 +1,11 @@
 from .compose import Compose
 from .crop_pad import Crop, Pad
+from .identity import Identity
 from .lambd import Lambda
 from .normalize import Normalize
+from .random_apply import RandomApply
 from .random_crop import RandomCrop
+from .random_rot_crop import RandomRotCrop
 from .resize import Resize
 from .to_tensor_pil_image import ToPILImage, ToTensor
+from .transform_if import TransformIf
