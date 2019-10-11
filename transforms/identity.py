0362d658f4b46529cd8b6b40ec0a402d73d1fe9e
Gregoire Clement <gregoire.clement@epfl.ch>
2019-10-12 00:01:37 +0900

    ADD Identity._invert implemented for better __repr__ of its revert

--- transforms/identity.py
+++ transforms/identity.py
@@ -1,12 +1,15 @@
-from . import Lambda
+from transforms import Lambda
 
 
 class Identity(Lambda):
     def __init__(self):
         super().__init__(
             lambd=lambda x: x,
-            lambd_inv=lambda x: x,
+            lambd_inv=lambda x: x,  # not used because _invert is overridden (for __repr__)
         )
 
     def __repr__(self):
         return 'Lambda x: x'
+
+    def _invert(self):
+        return Identity()
