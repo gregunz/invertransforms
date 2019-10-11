2847bce4da721fd1e8a2c1d237e636284dc087bf
gregunz <mail@gregunz.io>
2019-10-11 18:49:40 +0900

    ADD Resize transformation

---
+++ transforms/resize.py
@@ -0,0 +1,37 @@
+import warnings
+
+from torchvision import transforms
+
+from transforms.util import UndefinedInvertible
+
+
+class Resize(transforms.Resize, UndefinedInvertible):
+    img_w = img_h = None
+
+    def __call__(self, img):
+        """
+        Args:
+            img (PIL Image): Image to be scaled.
+
+        Returns:
+            PIL Image: Rescaled image.
+        """
+        self.img_w, self.img_h = img.size
+        return super().__call__(img)
+
+    def _invert(self):
+        return Resize(size=(self.img_h, self.img_w), interpolation=self.interpolation)
+
+    def _can_invert(self):
+        return self.img_w is not None and self.img_h is not None
+
+
+class Scale(Resize):
+    """
+    Note: This transform is deprecated in favor of Resize.
+    """
+
+    def __init__(self, *args, **kwargs):
+        warnings.warn("The use of the transforms.Scale transform is deprecated, " +
+                      "please use transforms.Resize instead.")
+        super().__init__(*args, **kwargs)
