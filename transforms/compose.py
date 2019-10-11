23456ccd75036eea1e66e0759fbdbeee5a66d5f5
gregunz <mail@gregunz.io>
2019-10-11 18:48:43 +0900

    ADD Compose Transformation

---
+++ transforms/compose.py
@@ -0,0 +1,12 @@
+from torchvision import transforms
+
+from transforms.util import Invertible, _Invertible
+
+
+class Compose(transforms.Compose, Invertible):
+    def _invert(self):
+        transforms_inv = []
+        for t in self.transforms[::-1]:
+            assert isinstance(t, _Invertible), f'{t.__class__.__name__} is not an invertible class'
+            transforms_inv.append(t.invert())
+        return Compose(transforms=transforms_inv)
