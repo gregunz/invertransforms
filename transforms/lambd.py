24a798fb0f69411c24e2421ba94f973d3fb546a0
gregunz <mail@gregunz.io>
2019-10-11 18:50:55 +0900

    ADD Lambda transformation

---
+++ transforms/lambd.py
@@ -0,0 +1,18 @@
+from torchvision import transforms
+
+from transforms.util import Invertible
+
+
+class Lambda(transforms.Lambda, Invertible):
+    def __init__(self, lambd, lambd_inv=None):
+        super().__init__(lambd)
+        if lambd_inv is None:
+            lambd_inv = lambd  # todo: what's best ? -no default, -lambda x:x, lambd_inv=lambd
+        assert callable(lambd_inv), repr(type(lambd_inv).__name__) + " object is not callable"
+        self.lambd_inv = lambd_inv
+
+    def _invert(self):
+        return Lambda(
+            lambd=self.lambd_inv,
+            lambd_inv=self.lambd
+        )
