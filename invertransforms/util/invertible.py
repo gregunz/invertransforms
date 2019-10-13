7b36d6d5f7e1198ec39adf24588d2b41be5ff02a
Gregoire Clement <gregoire.clement@epfl.ch>
2019-10-13 18:57:02 +0900

    ADD feature: replay a transformation

--- invertransforms/util/invertible.py
+++ invertransforms/util/invertible.py
@@ -2,6 +2,11 @@ from abc import ABC, abstractmethod
 
 
 class Invertible(ABC):
+
+    @abstractmethod
+    def __call__(self, img):
+        raise NotImplementedError
+
     @abstractmethod
     def invert(self):
         raise NotImplementedError
@@ -14,6 +19,23 @@ class Invertible(ABC):
         :return:
         """
         return self.invert()(img)
+
+    def replay(self, img):
+        """
+        Replay a transformation (with random like previous runs).
+        If it is called before any calls to __call__, it will simply calls __call__
+
+        Note: Any call to __call__ will change the randomness again.
+
+        :param img:
+        :return:
+        """
+        try:
+            # hack: because inverse fixes the randomness,
+            #       we can replay for free with double inverse
+            return self.invert().invert().__call__(img)
+        except InvertibleException:
+            return self.__call__(img)
 
     def __repr__(self):
         return f'{self.__class__.__name__}()'
--- tests/util/test_invertible.py
+++ tests/util/test_invertible.py
@@ -1,24 +1,48 @@
-import unittest
+import torch
 
+import invertransforms as T
 from invertransforms.util import Invertible
+from tests.invertible_test_case import InvertibleTestCase
 
 
-class TestInvertible(unittest.TestCase):
+class TestInvertible(InvertibleTestCase):
+    def setUp(self) -> None:
+        super().setUp()
 
-    def test_class(self):
+        # This test is used for accessing abstract methods
         class NewInvertible(Invertible):
-            pass
-
-        with self.assertRaises(TypeError):
-            NewInvertible()
-
-        # This test is used for accessing abstract method
-        class NewInvertibleBis(Invertible):
             def invert(self):
                 super().invert()
 
-        tf = NewInvertibleBis()
-        with self.assertRaises(NotImplementedError):
-            tf.invert()
+            def __call__(self, img):
+                super().__call__(img)
 
-        self.assertIn(tf.__class__.__name__, repr(tf))
+        self.tf = NewInvertible()
+
+    def test_instantiate_abstract(self):
+        class NewInvertibleBis(Invertible):
+            pass
+
+        with self.assertRaises(TypeError):
+            NewInvertibleBis()
+
+    def test_access_abstract_methods(self):
+        with self.assertRaises(NotImplementedError):
+            self.tf(None)
+
+        with self.assertRaises(NotImplementedError):
+            self.tf.invert()
+
+    def test_replay(self):
+        tf = T.Compose([
+            T.RandomCrop(self.crop_size),
+            T.RandomVerticalFlip(),
+            T.RandomHorizontalFlip(),
+            T.ToTensor(),
+        ])
+        img_tf1 = tf.replay(self.img_pil)
+        img_tf2 = tf.replay(self.img_pil)
+        self.assertTrue(torch.allclose(img_tf1, img_tf2))
+
+    def test_repr(self):
+        self.assertIn(self.tf.__class__.__name__, repr(self.tf))
