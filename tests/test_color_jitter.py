8079c0e34d6a2a5f3c949fab4f1f359b8bfea254
Gregoire Clement <gregoire.clement@epfl.ch>
2019-10-14 17:16:09 +0900

    TEST more

--- tests/test_color_jitter.py
+++ tests/test_color_jitter.py
@@ -1,8 +1,14 @@
 import invertransforms as T
+from invertransforms.util import InvertibleError
 from tests.invertible_test_case import InvertibleTestCase
 
 
 class TestColorJitter(InvertibleTestCase):
+
+    def test_invert_before_apply(self):
+        with self.assertRaises(InvertibleError):
+            T.ColorJitter().inverse()
+
     def test_invert_is_identity(self):
         tf = T.ColorJitter()
         img_tf = tf(self.img_pil)
